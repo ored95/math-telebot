29fce69d0ed607a60e9fccc90a4f6d1ff1633d5a
Binh D. Nguyen <stepup.ored@gmail.com>
2022-04-30 02:52:27 +0300

    special function handler and monotonic ranges problem

--- handler/equation.py
+++ handler/equation.py
@@ -1,82 +1,131 @@
 """
 Equation solution handler
 """
-from tkinter import RIGHT
 from function import Function
-from enum import Enum
-class order(Enum):
-    LEFT=-1
-    RIGHT=+1
+from enum import IntEnum
 
 class Equation:
     def __init__(self, txt):
         # preprocessing 
         txt = txt.lower().replace('=', '-(') + ')'
-        self.func = Function.extract(txt)
-        self.xinit = 1e-3
+        txt = txt.replace(' ', '')
+        try:
+            self.func = Function.extract(txt)
+            self.eps = 1e-5
+            self.xinit = 1e-3
+            self.ranges = []
+            self.get_ranges()
+            print(self.ranges)
+            
+            self.roots = []
+            # if abs(self.func(0)) <= self.eps:
+            #     self.roots.append(0)
+                
+            self.binary_searching()
+            
+            # clean same roots
+            i = 1
+            while i < len(self.roots):
+                if abs(self.roots[i-1] - self.roots[i]) < self.eps:
+                    self.roots.remove(self.roots[i])
+                else:
+                    i += 1
+            print(self.roots)
+        except SyntaxError:
+            print("Error: can not recognize that function")
 
     def same_sign(self, a, b):
+        """
+        check if 2 numbers have same signs
+        """
         return (a >= 0) ^ (b < 0)
 
-    def search_border(self, x, order=order.LEFT, sign=1):
+    def search_border(self, x, order=1, sign=1, maxTimes=15):
         """
         sign: -1 means negative else 1 - positive
         """
         dx = self.xinit
-        while self.same_sign(sign, self.func(x)):
+        time = 1
+        while time <= maxTimes and self.same_sign(sign, self.func(x)):
             dx *= 2
+            time += 1
             x = x + order * dx
         return x
 
-    # def get_range(self, xstart, xend, maxRange=1e3):
-    #     fs, fe = -1, 1  # by default: sign=-1 means < 0, 
-    #     xs, xe = self.left_border(xstart), self.right_border(xend)
-    #     if xe - xs < maxRange:
-    #         self.ranges.push_back([xs, xe])
-    #     return xs, xe
+    # def get_ranges(self, maxRange=1e3):
+    #     """_summary_
+    #         Extract all monotonic ranges
+    #     Args:
+    #         maxRange (_type_, optional): _description_. Defaults to 1e3.
+    #     """
+    #     def get_sign(value):
+    #         """
+    #         define sign of value, -1 when negative, otherwise +1
+    #         """
+    #         return -1 if value < 0 else 1
+        
+    #     # init part
+        
+    #     sign = get_sign(self.func(-self.xinit))
+    #     xl0 = self.search_border(self.xinit, -1, sign)
+    #     xr0 = self.search_border(self.xinit, +1, sign)
 
-    def get_ranges(self, maxRange=1e6):
-        self.ranges = []
-        # init part
-        sign = -1 if (self.func(-self.xinit) < 0) else 1
-        sig0 = -sign
-        xl0 = -self.xinit
-        xr0 = self.search_border(self.xinit, sign)
-        if xr0 - xl0 < maxRange:
-            self.ranges.push_back([xl0, xr0])
+    #     if xr0 - xl0 < maxRange and not self.same_sign(self.func(xr0), self.func(xl0)):
+    #         self.ranges.append([xl0, xr0])
 
-        # left part
-        xr = xr0
-        xl = self.search_border(xr, order.LEFT, sign)
-        while abs(xl) < maxRange:
-            self.ranges.push_back([xl, xr])
-            sign = -sign
-            xr = xl
-            xl = self.search_border(xr, order.LEFT, sign)
-
-        # right part
-        sign = sig0 
-        xl = xl0
-        xr = self.search_border(xl, order.RIGHT, sign)
-        while abs(xr) < maxRange:
-            self.ranges.push_back([xl, xr])
-            sign = -sign
-            xl = xr
-            xr = self.search_border(xl, order.RIGHT, sign)
- 
-    def binary_search(self, maxIter=1000):
-        """
-        Return:
-            all monotonous part of function
-        """    
-        for n in range(maxIter):
-            pass
-
-    def newton(self, xstart=-100, xend=100):
+    #     # left part: order=-1
+    #     xr = xr0
+    #     xl = self.search_border(xr, -1, sign)
+    #     while abs(xl) < maxRange and not self.same_sign(self.func(xr), self.func(xl)):
+    #         self.ranges.append([xl, xr])
+    #         sign = -sign
+    #         xr = xl
+    #         xl = self.search_border(xr, -1, sign)
+        
+    #     # right part: order=+1
+    #     sign = get_sign(self.func(xl0))
+    #     xl = xl0
+    #     xr = self.search_border(xl, +1, sign)
+    #     while abs(xr) < maxRange and not self.same_sign(self.func(xr), self.func(xl)):
+    #         self.ranges.append([xl, xr])
+    #         sign = -sign
+    #         xl = xr
+    #         xr = self.search_border(xl, +1, sign)
+    def get_ranges(self, xmin=-1e3, xmax=1e3):
+        def get_sign(value):
+            """
+            define sign of value, -1 when negative, otherwise +1
+            """
+            return -1 if value < 0 else 1
+        
+        x, sgx = xmin, get_sign(self.func(xmin))
+        dx = 0.1/3      # magic number
+        while x <= xmax:
+            if not self.same_sign(self.func(x), self.func(x+dx)):
+                self.ranges.append([x, x+dx])
+            x += dx
+        
+    def newton(self):
         pass
+    
+    def binary_searching(self, feps=1e-3):
+        for xb, xe in self.ranges:
+            xm = (xb + xe) / 2.
+            fb, fm, fe = self.func(xb), self.func(xm), self.func(xe)
+            while xe - xb > self.eps:
+                if self.same_sign(fb, fm):
+                    xb, fb = xm, fm
+                else:
+                    xe, fe = xm, fm
+                xm = (xb + xe) / 2.
+                fm = self.func(xm)
+            if abs(fm) < feps:
+                self.roots.append(xm)
 
-e = Equation("(x^2 - 1)(6x-2/x)=7x^3/(3x+2)-2")
-e.disp()
-
-func = lambda x: (x ** 2 - 1)*(6*x-2/x)-7*x**3/(3*x+2)+2
-print(func(1))
+# e = Equation("2^x/x^3=x+1")
+# e = Equation("x^3-3x^2=-3x+1")
+# e = Equation("x^2=1")
+# e = Equation("x^2=3x-2")
+# e = Equation("t-sin(t)=1")
+# e = Equation("exp(t)=t+1")
+e = Equation("7x^2-x+3 = 4x^4+1")
--- handler/function.py
+++ handler/function.py
@@ -1,30 +1,43 @@
 """
 Function extraction handler
 """
+from math import sin, cos, tan, exp, pi
 class Function:
     @staticmethod
     def extract(txt):
         """
         Eval handler without exception
+        TODO: log
         """
+        # special function
+        spec_funcs = ["sin", "cos", "tan", "exp", "pi"]
         # first search for variable
         def is_alpha(c):
             return c >= 'a' and c <= 'z'
         
         i = 0
-        while not is_alpha(txt[i]):
-            i += 1
-        j = i+1
-        while is_alpha(txt[j]):
-            j += 1
+        while True:
+            while not is_alpha(txt[i]):
+                i += 1
+            j = i+1
+            while is_alpha(txt[j]):
+                j += 1
+            if txt[i:j] not in spec_funcs:
+                break
+            else:
+                i = j
+        
         txt = txt.replace(txt[i:j], "x")
         tmp = txt[0]
         for i in range(1, len(txt)):
             if not is_alpha(txt[i-1]) and txt[i] == 'x':
                 tmp += '*'
             tmp += txt[i]
+        
         tmp = tmp.replace('^', '**')    # x^...
         tmp = tmp.replace('/*', '/')    # 1/x
+        tmp = tmp.replace('+*', '+')    # +x
+        tmp = tmp.replace('-*', '-')    # -x
         tmp = tmp.replace('(*', '(')    # (x ...)
         tmp = tmp.replace('***', '**')  # ...^x
         tmp = tmp.replace(')(', ')*(')  # ...)(...
