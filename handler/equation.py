c94e5a57fd08319f7f4b292165df5ee5cac70049
Binh D. Nguyen <stepup.ored@gmail.com>
2022-05-02 16:34:46 +0300

    equation with exponent fixed (overflow error)

--- handler/equation.py
+++ handler/equation.py
@@ -16,25 +16,43 @@ class Equation:
         except SyntaxError:
             return None
 
-def search_intervals(f, lhs=-1e3, rhs=1e3, dx=0.083, eps=1e-10):
-    intervals = []
-    x1, x2 = lhs, lhs+dx
-    f1, f2 = f(x1), f(x2)
-    while x2 <= rhs:
+def is_interval(f, x1, x2, eps):
+    try:
+        f1, f2 = f(x1), f(x2)
         if abs(f2) < eps:
-            intervals.append([x2, x2])
-            x1 = x2+dx
-            f1 = f(x1)
-        else:
-            if abs(f1) < eps:
-                intervals.append([x1, x1])    
-            elif f1 * f2 < 0:
-                intervals.append([x1, x2])
-            x1, f1 = x2, f2
-        x2 = x1+dx
-        f2 = f(x2)
-    return intervals
+            return [x2, x2]
+        if abs(f1) < eps:
+            return [x1, x1]
+        if f1 * f2 < 0:
+            return [x1, x2]
+        if abs(f1 * f2) < 1e-3:
+            # binary searching
+            xm = (x1 + x2) / 2.
+            fm = f(xm)
+            while abs(x1-x2) > eps:
+                if abs(abs(f1) - max(abs(fm), abs(f1), abs(f2))) < eps:
+                    x1, f1 = xm, fm
+                else:
+                    x2, f2 = xm, fm
+                xm = (x1 + x2) / 2.
+                fm = f(xm)
+            if abs(fm) < eps:
+                return [xm, xm]
+        return None
+    except:
+        return None
 
+def search_intervals(f, lhs, rhs, dx=0.083, eps=1e-10):
+    intervals = []
+    x = lhs
+    while x < rhs:
+        res = is_interval(f, x, x+dx, eps)
+        if res is not None:
+            intervals.append(res)
+            x = res[0]
+        x += dx
+    return intervals
+        
 def root(f, method, intervals, eps=1e-10):
     roots = []
     for lhs, rhs in intervals:
@@ -49,7 +67,7 @@ def root(f, method, intervals, eps=1e-10):
 # TODO
 # e = Equation("t-sin(t)=1")    # periodic function
 # e = Equation("exp(t)=t+1")    # OverflowError: math range error
-equations = ["7x^2-x+3 = 4x^4+1", "x^2=3x-2", "x^2=1", "x^3-3x^2=-3x+1", "2^x/x^3=x+1", "1/x=3"]
+equations = ["exp(t)=t+1"] #"7x^2-x+3 = 4x^4+1", "x^2=3x-2", "x^2=1", "x^3-3x^2=-3x+1", "2^x/x^3=x+1", "1/x=3"]
 for eqn in equations:
     f = Equation.func(eqn)
     if f is not None:
